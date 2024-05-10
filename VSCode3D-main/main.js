e8d93f82dda3cb723adb84008502820f3f3d535e
turtleloud <146945920+turtleloud@users.noreply.github.com>
2024-05-10 09:27:19 -0300

    Add files via upload

--- VSCode3D-main/main.js
+++ VSCode3D-main/main.js
@@ -3,7 +3,17 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1;
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+    return dias;
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo) 
+
 
 for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
