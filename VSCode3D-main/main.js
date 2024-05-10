4563634102a7e832fbde13753a7ed8cb3d3b737b
turtleloud <146945920+turtleloud@users.noreply.github.com>
2024-05-09 23:21:35 -0300

    Add files via upload

---
+++ VSCode3D-main/main.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1;
+
+for (let i=0; i<botoes.length;i++){
+    botoes[i].onclick=function(){
+        for(j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
